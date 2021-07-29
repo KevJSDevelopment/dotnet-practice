884e73f3435792a873e10a1d3c3acab28aaf64a1
KevJSDevelopment <Kshhn12@gmail.com>
2021-07-28 21:03:49 -0400

    entity relationships built

--- Domain/User.cs
+++ Domain/User.cs
@@ -1,10 +1,11 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 
 namespace Domain
 {
     public class User : IdentityUser
     {
-
+        public ICollection<ActivityAttendee> Activities { get; set; }
 
     }
 }
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,5 +12,23 @@ namespace Persistence
         }
 
         public DbSet<Activity> Activities { get; set; }
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new {aa.UserId, aa.ActivityId}));
+
+            builder.Entity<ActivityAttendee>()
+            .HasOne(u => u.User)
+            .WithMany(a => a.Activities)
+            .HasForeignKey(aa => aa.UserId);
+
+            builder.Entity<ActivityAttendee>()
+            .HasOne(act => act.Activity)
+            .WithMany(att => att.Attendees)
+            .HasForeignKey(aa => aa.ActivityId);
+        }
     }
 }
